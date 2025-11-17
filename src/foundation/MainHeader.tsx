6d3cbfae55068d67737474a7fe8998887d267270
Eduardo Donelli Pellenz <49295044+donelli@users.noreply.github.com>
2025-11-17 17:55:33 -0300

    feat: improve header UI

--- src/foundation/MainHeader.tsx
+++ src/foundation/MainHeader.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "~/providers/auth-provider";
 import { Avatar } from "./Avatar";
 import styles from "./MainHeader.module.css";
 import { MdLogout } from "react-icons/md";
+import Image from "next/image";
 
 export function MainHeader() {
   const { user, logout } = useAuth();
@@ -17,12 +18,13 @@ export function MainHeader() {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <Link href="/">
+        <Link href="/" className={styles.logo}>
+          <Image src="/logo.png" alt="Handoff" width={20} height={20} />
           <h1 className={styles.title}>Handoff Take Home</h1>
         </Link>
       </div>
       <div className={styles.right}>
-        {userInitial && <Avatar>{userInitial}</Avatar>}
+        {userInitial && <Avatar size={24}>{userInitial}</Avatar>}
         <span className={styles.userName}>{user?.name}</span>
         <span
           className={styles.logoutButton}
